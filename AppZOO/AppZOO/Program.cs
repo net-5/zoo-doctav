32b2e5d108919b36bcaa74a1dffc80a0377dec44
doctav <doctav@uaic.ro>
2019-07-06 20:40:42 +0300

    Initial commit

---
+++ AppZOO/AppZOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppZOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
